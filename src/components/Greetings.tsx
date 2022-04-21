758d16c5f38603b1c3c7d284726bc5f14d464bd6
asdf <kgh2703@irlink.co.kr>
2022-04-21 13:55:31 +0900

    1. 컴포넌트 수정

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
 /**
- * 컴포넌트 props type을 선언할 때는 type이나 interface를 써야하며 일관성을 유지해야 한다.
+ * 컴포넌트의 props중 생략해도 되는 값이 존재하면 변수 뒤에 ?를 붙여준다.
  */
 interface GreetingsProps {
     name: string;
     desc: string;
+    optional?: string;
 };
 
 /**
  * React.FC를 생략하면 defaultProps는 잘 동작한다.
  */
-const Greetings = ({ name, desc }: GreetingsProps) => {
+const Greetings = ({ name, desc, optional }: GreetingsProps) => {
     return (
         <div>
             {name}<br />
-            {desc}
+            {desc}<br />
+            {
+                optional && <p>{optional}</p>
+            }
         </div>
     );
 };
